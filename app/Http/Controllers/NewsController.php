b8e97fd5f1fe40b48dfb6779b7ccc0e06f5f7cab
aregghaz@gmail.com <gagulik014>
2019-11-25 11:53:00 +0400

    bug

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -152,8 +152,6 @@ class NewsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param int $id
-     * @return Response
      */
     public function destroy($id)
     {
